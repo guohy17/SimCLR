bd44f2a292c9463550e9ec2199eb26325b6d707b
guohy17 <38449178+guohy17@users.noreply.github.com>
2020-06-17 22:42:28 +0800

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -5,6 +5,48 @@ import torchvision.models as models
 from DropBlock import DropBlock2D
 
 
+class Bottleneck(nn.Module):
+    def __init__(self, encode_num, inplanes, planes, stride=1, expansion=4):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * expansion, kernel_size=1)
+        self.bn3 = nn.BatchNorm2d(planes * expansion)
+        self.relu = nn.ReLU(True)
+        if encode_num == 0:
+            self.dropblock = DropBlock2D(drop_prob=0.02, block_size=7)
+        elif encode_num == 3:
+            self.dropblock = DropBlock2D(drop_prob=0.1, block_size=3)
+        else:
+            self.dropblock = DropBlock2D(drop_prob=0.05, block_size=5)
+        if stride != 1 or inplanes != expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inplanes, expansion * planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(expansion * planes)
+            )
+
+    def forward(self, x):
+        shortcut = self.shortcut(x) if hasattr(self, "shortcut") else x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.dropblock(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        x += shortcut
+        x = self.relu(x)
+        return x
+
+
 class ResNet(nn.Module):
     def __init__(self, opt, block=Bottleneck, input_dims=3):
         super(ResNet, self).__init__()
